a60637fdb90d4372cdda0858c9b14ccf67be66eb
WaigoonN <21552@satitpatumwan.ac.th>
2024-07-22 19:40:40 +0700

    Changed font weight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet"/>
       </head>
       <body>{children}</body>
     </html>
