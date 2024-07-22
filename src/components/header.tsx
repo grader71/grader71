0395d7fbf634133de6db10a863d36629b3a69ebd
WaigoonN <21552@satitpatumwan.ac.th>
2024-07-22 16:51:44 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,24 +3,14 @@ import '@/styles/header.css'
 const Header = ({ selected, username }: { selected: string, username: string }) => {
     return (
         <div className="navigation_bar">
-            <div className="logo">
+            {/* <div className="logo">
                 (logo)
-            </div>
-            <div className={"Home" === selected ? "menu_selected" : "menu"}>
-                <a className="header_link" href="/">Home</a>
-            </div>
-            <div className={"Problems" === selected ? "menu_selected" : "menu"}>
-                <a className="header_link" href="/Problems">Problems</a>
-            </div>
-            <div className={"Submissions" === selected ? "menu_selected" : "menu"}>
-                <a className="header_link" href="/Submissions">Submissions</a>
-            </div>
-            <div className={"About" === selected ? "menu_selected" : "menu"}>
-                <a className="header_link" href="/About">About</a>
-            </div>
-            <div className={"Profile" === selected ? "menu_selected" : "menu"}>
-                <a className="header_link" href="/Proflie">{username}</a>
-            </div>
+            </div> */}
+            <a className={"Home" === selected ? "menu_selected" : "menu"} href="/">Home</a>
+            <a className={"Problems" === selected ? "menu_selected" : "menu"} href="/Problems">Problems</a>
+            <a className={"Submissions" === selected ? "menu_selected" : "menu"} href="/Submissions">Submissions</a>
+            <a className={"About" === selected ? "menu_selected" : "menu"} href="/About">About</a>
+            <a className={"Profile" === selected ? "account_menu_selected" : "account_menu"} href="/Proflie">{username}</a>
         </div>
     );
 }
