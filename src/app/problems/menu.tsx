92dee22ecd28c860a7b1c7cc1c371a10add2d27a
PakinDioxide <pakindioxide@gmail.com>
2024-07-24 22:29:05 +0700

    Fix level menu bar

---
+++ src/app/problems/menu.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React, { useState } from 'react';
+import Header from '@/components/header';
+import '@/styles/global.css';
+import '@/styles/problems.css';
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+
+const levels = [
+    {
+        label: 'All',
+        value: ''
+    },
+    {
+        label: 'Camp 1',
+        value: 'camp1'
+    },
+    {
+        label: 'Camp 2',
+        value: 'camp2'
+    },
+    {
+        label: 'TOI',
+        value: 'toi'
+    }
+];
+
+const Menu = () => {
+    const searchParams = useSearchParams();
+    const level = searchParams.get('level') || '';
+
+    return (
+        <div className='problems_box'>
+            <div className='problems_menu_box'>
+                {
+                    levels.map(levelItem => {
+                        const href = levelItem.value === '' ? '/problems' : `/problems?level=${levelItem.value}`;
+                        return (
+                            <Link
+                                key={levelItem.value}
+                                href={href}
+                                className={level === levelItem.value ? 'problems_menu_selected' : 'problems_menu'}
+                            >{levelItem.label}</Link>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Header from '@/components/header'
 import '@/styles/global.css'
 import '@/styles/problems.css'
+import Menu from './menu'
 
 const Problems = () => {
   const [selected, setSelected] = useState("Camp 1")
@@ -11,13 +12,7 @@ const Problems = () => {
     <div className='width_small'>
       <Header selected = "Problems" username = "PakinDioxide"/>
       <p className='topic'>Problems</p>
-      <div className='problems_box'>
-        <div className='problems_menu_box'>
-          <button className={"Camp 1" === selected ? "problems_menu_selected" : "problems_menu"} onClick={ () => setSelected("Camp 1")}>Camp 1</button >
-          <button className={"Camp 2" === selected ? "problems_menu_selected" : "problems_menu"} onClick={ () => setSelected("Camp 2")}>Camp 2</button >
-          <button className={"TOI" === selected ? "problems_menu_selected" : "problems_menu"} onClick={ () => setSelected("TOI")}>TOI</button>
-        </div>
-      </div>
+      <Menu />
     </div>
   )
 }
