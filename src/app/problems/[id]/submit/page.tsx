c8435d8e2992ba7174c402b1cc0cc1a1ca4d2b9f
WaigoonN <cuswaigoon@gmail.com>
2024-08-11 12:29:53 +0700

    Added submit button

--- src/app/problems/[id]/submit/page.tsx
+++ src/app/problems/[id]/submit/page.tsx
@@ -6,32 +6,42 @@ import '@/styles/problems.css'
 import ProblemMenu from '../ProblemMenu'
 import CodeMirror from '@uiw/react-codemirror'
 import { langs, loadLanguage } from '@uiw/codemirror-extensions-langs'
-import { tokyoNight } from '@uiw/codemirror-themes-all';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 
 const Problems = ({ params } : { params: {id: string }}) => {
   const problem_id = params.id
 
-  loadLanguage('cpp')
+  var contents: string = `#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n  \n}`;
 
-  const onChange = React.useCallback((value: any, viewUpdate: any) => {
-    console.log("value:", value);
-  }, []);
+  const getContents = () => {
+    if (!contents) {
+      alert('No code submitted')
+    }
+    console.log(contents)
+  }
+
+  loadLanguage('cpp')
 
   return (
     <div className='width_small'>
       <Header selected = "Problems" username = "PakinDioxide"/>
       <p className='topic'>Problems Name <span className='small_topic'>({problem_id})</span></p>
-      <div className='problems_box' style={{height:"450px"}}>
+      <div className='problems_box' style={{height:"600px"}}>
         <Suspense><ProblemMenu problem_id={problem_id} selected='submit'/></Suspense>
         <CodeMirror
           className='submit_code_editor'
-          value={`#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n\n}`}
+          value={contents}
           theme={tokyoNight}
           extensions={[langs.cpp()]}
-          onChange={(value, viewUpdate) => {
-            console.log(value)
-          }}
+          onChange={(value) => {
+            contents = value
+            }
+          }
         />
+        <button
+          className='submit_button'
+          onClick={getContents}
+          >Submit</button>
       </div>
     </div>
   )
