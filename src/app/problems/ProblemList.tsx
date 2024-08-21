f532945fffedf7d3c35672f2a69343f4e81ce312
WaigoonN <cuswaigoon@gmail.com>
2024-08-21 22:03:44 +0700

    Fixed useSearchParams Attempt 1

--- src/app/problems/ProblemList.tsx
+++ src/app/problems/ProblemList.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 function ProblemList() {
     const problems = [
         {
--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState , Suspense } from 'react'
+import Link from 'next/link';
 import Header from '@/components/header'
 import '@/styles/global.css'
 import '@/styles/problems.css'
@@ -10,6 +11,9 @@ import { useSearchParams } from 'next/navigation';
 const problems = ProblemList();
 
 const Problems = () => {
+  const searchParams = useSearchParams();
+  const level = searchParams.get('level') || '';
+  
   return (
     <div className='width_small'>
       <Header selected = "Problems" username = "PakinDioxide"/>
@@ -19,19 +23,16 @@ const Problems = () => {
         <div className='problems_container'>
           {
             problems.map(item => {
-              const searchParams = useSearchParams();
-              const level = searchParams.get('level') || '';
-
               const href = `/problems/${item.id}`
               if (level === '' || item.level == level) {
                 return (
-                <a className='problem_display' href={href} key={item.id}>
+                <Link className='problem_display' href={href} key={item.id}>
                   <div className="problem_frame">
                     <p style={{fontSize: "20px", marginRight: "20px"}}>{item.name}</p>
                     <p style={{fontSize: "12px"}}>[{item.id}]</p>
                     <p style={{fontSize: "20px", textAlign: "right", flex: 1}}>{item.score}/100</p>
                   </div>
-                </a>
+                </Link>
                 )
               }
             })
